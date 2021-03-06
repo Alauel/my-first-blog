bd72ab753b366c6154d9777b64aa89d3c6b22d73
alauel <caelli@zoho.com>
2021-03-06 18:58:14 +0800

    Added Registration Form

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+
+from django.conf.urls import url
+
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -9,4 +14,5 @@ urlpatterns = [
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('accounts/signup/',views.signup,name="signup")
 ]
