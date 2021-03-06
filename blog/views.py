2ad0a9a03e3e7042d8d23d8911572bfa75a65859
alauel <caelli@zoho.com>
2021-03-06 13:27:18 +0800

    Fixed Login

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
-@login_required
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
